d1b467ca855586705811b6de53aa42922ac5a2f2
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-06-11 18:29:10 +0530

    solves ongest common prefix

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+
+    }
+
+    public String longestCommonPrefix(String[] array) {
+        if (array.length == 0) {
+            return "";
+        }
+
+        String result = array[0];
+        for (int index = 1 ; index < array.length ; index++) {
+            result = longestCommonPrefix(result, array[index]);
+        }
+        return result;
+    }
+
+    private String longestCommonPrefix(String first, String second) {
+        for (int index = 0 ; index < first.length() && index < second.length() ; index++) {
+            if (first.charAt(index) != second.charAt(index)) {
+                return first.substring(0, index);
+            }
+        }
+        return first.length() < second.length() ? first : second;
+    }
+}
