6fe399852fb27e41eb740e8641f040deaf0d401d
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-08-12 18:27:48 +0530

    minimu depth of binary tree

---
+++ src/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,26 @@
+public class MinimumDepthOfBinaryTree {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (isLeafNode(root)) {
+            return 1;
+        }
+
+        int leftDepth = root.left == null ? Integer.MAX_VALUE : minDepth(root.left);
+        int rightDepth = root.right == null ? Integer.MAX_VALUE : minDepth(root.right);
+
+        return 1 + Math.min(leftDepth, rightDepth);
+    }
+
+    private static boolean isLeafNode(TreeNode root) {
+        return root.left == null && root.right == null;
+    }
+}
