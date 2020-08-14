37704a944c6fd8cf5a56ff12e06862c10685538c
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-08-15 02:06:49 +0530

    solves palindrome linked list

---
+++ src/PalindromeLinkedList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromeLinkedList {
+    private static class ListNode {
+        int val;
+        ListNode next;
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        List<Integer> list = listFrom(head);
+        return isPalindrome(list);
+    }
+
+    private static boolean isPalindrome(List<Integer> list) {
+        for (int index = 0 ; index < list.size() / 2 ; index++) {
+            if (!list.get(index).equals(list.get(list.size() - 1 - index))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<Integer> listFrom(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        return list;
+    }
+}
