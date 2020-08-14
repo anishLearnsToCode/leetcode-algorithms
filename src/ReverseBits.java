1311eee59e80cee778456288a037fbee1839c6dc
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-08-14 22:28:52 +0530

    solves reverse bits

---
+++ src/ReverseBits.java
@@ -0,0 +1,20 @@
+public class ReverseBits {
+    public int reverseBits(int n) {
+        for (int i = 0; i < 16; i++) {
+            n = swapBits(n, i, 32 - i - 1);
+        }
+
+        return n;
+    }
+
+    public int swapBits(int n, int i, int j) {
+        int a = (n >> i) & 1;
+        int b = (n >> j) & 1;
+
+        if ((a ^ b) != 0) {
+            return n ^= (1 << i) | (1 << j);
+        }
+
+        return n;
+    }
+}
