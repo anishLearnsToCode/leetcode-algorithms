a32dd26598238d7e4f9d34599eb29dd182903816
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-08-15 11:17:55 +0530

    solves valid anagram

---
+++ src/ValidAnagram.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        Map<Character, Integer> frequency1 = getFrequencies(s);
+        Map<Character, Integer> frequency2 = getFrequencies(t);
+        return frequency1.equals(frequency2);
+    }
+
+    private static Map<Character, Integer> getFrequencies(String string) {
+        Map<Character, Integer> frequency = new HashMap<>();
+        for (int index = 0 ; index < string.length() ; index++) {
+            frequency.put(string.charAt(index), frequency.getOrDefault(string.charAt(index), 0) + 1);
+        }
+        return frequency;
+    }
+}
