eef2d2c39f5c3f9876edb7800a352cc92596c35a
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-08-17 14:16:23 +0530

    solves is perfect square

---
+++ src/IsPerfectSquare.java
@@ -0,0 +1,12 @@
+public class IsPerfectSquare {
+    public static boolean isPerfectSquare(long number) {
+        long left = 0, right = number, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (mid * mid == number) return true;
+            else if (mid * mid < number) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+}
