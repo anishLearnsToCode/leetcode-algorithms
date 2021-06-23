cf8f462ad7635963369a28b5e8af7973c1fc98ce
anishLearnsToCode <anish_@outlook.com>
2021-06-23 18:24:08 +0530

    solves min depth binary tree in python

---
+++ python/minimum_depth_of_binary_tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        if root.left is None:
+            return 1 + self.minDepth(root.right)
+        if root.right is None:
+            return 1 + self.minDepth(root.left)
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
