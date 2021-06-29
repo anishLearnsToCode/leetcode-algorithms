cc6dc92b82c0ac8bf3beaeaa6b5333fa71582afe
anishLearnsToCode <anish_@outlook.com>
2021-06-29 18:45:53 +0530

    solves is subsequence in python

--- python/find_the_difference.py
+++ python/find_the_difference.py
@@ -1,17 +1,6 @@
-from typing import Dict
-
-
 class Solution:
-    def get_character_frequency(self, string: str) -> Dict[str, int]:
-        frequency = {}
-        for character in string:
-            frequency[character] = frequency.get(character, 0) + 1
-        return frequency
+    def ascii_sum(self, string: str) -> int:
+        return sum(ord(character) for character in string)
 
     def findTheDifference(self, s: str, t: str) -> str:
-        characters_s = self.get_character_frequency(s)
-        for character in t:
-            if characters_s.get(character, 0) == 0:
-                return character
-            else:
-                characters_s[character] -= 1
+        return chr(self.ascii_sum(t) - self.ascii_sum(s))
