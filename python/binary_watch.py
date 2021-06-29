abb6b278bc88bbfd94b6f17831f2906f78fe630a
anishLearnsToCode <anish_@outlook.com>
2021-06-29 20:44:09 +0530

    solves binary watch in python

---
+++ python/binary_watch.py
@@ -0,0 +1,5 @@
+class Solution:
+    def readBinaryWatch(self, num):
+        return ['%d:%02d' % (h, m)
+                for h in range(12) for m in range(60)
+                if (bin(h) + bin(m)).count('1') == num]
