522c2b1cd263fe16bbec80e4b790a9219338eed5
Anish Sachdeva <anish_@outlook.com>
2021-12-21 18:15:54 +0100

    a

---
+++ src/CheckIfOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CheckIfOneStringSwapCanMakeStringsEqual {
+    public boolean areAlmostEqual(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        final Queue<Integer> differencesIndex = new LinkedList<>();
+        for (int i = 0 ; i < s1.length() ; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                differencesIndex.add(i);
+            }
+        }
+        return differencesIndex.size() == 0 || (differencesIndex.size() == 2 && areSameChars(s1, s2, differencesIndex));
+    }
+
+    private boolean areSameChars(String s1, String s2, Queue<Integer> differenceIndexes) {
+        
+    }
+}
