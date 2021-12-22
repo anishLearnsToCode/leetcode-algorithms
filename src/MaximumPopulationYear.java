0088d0284a0048d536e16b78b6849773f2b73490
anishLearnsToCode <anish_@outlook.com>
2021-12-22 23:14:30 +0100

    solves maximum population year

---
+++ src/MaximumPopulationYear.java
@@ -0,0 +1,22 @@
+public class MaximumPopulationYear {
+    public int maximumPopulation(int[][] logs) {
+        final int[] population = new int[101];
+        for (int[] log : logs) {
+            population[log[0] - 1950]++;
+            population[log[1] - 1950]--;
+        }
+        return maxIndexOfRunningSum(population) + 1950;
+    }
+
+    private int maxIndexOfRunningSum(int[] array) {
+        int maxIndex = 0;
+        for (int index = 1, current = array[0], maxVal = current ; index < array.length ; index++) {
+            current += array[index];
+            if (current > maxVal) {
+                maxVal = current;
+                maxIndex = index;
+            }
+        }
+        return maxIndex;
+    }
+}
