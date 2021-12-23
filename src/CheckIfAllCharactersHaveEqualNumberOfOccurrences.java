f852c6f66719a066a250e61ff28e5c913ea49fc0
anishLearnsToCode <anish_@outlook.com>
2021-12-23 19:54:07 +0100

    solves check if all chars have same frequency

---
+++ src/CheckIfAllCharactersHaveEqualNumberOfOccurrences.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences
+// T: O(|s|)
+// S: O(1)
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckIfAllCharactersHaveEqualNumberOfOccurrences {
+    public boolean areOccurrencesEqual(String s) {
+        final Map<Character, Integer> charFrequencies = getCharacterFrequencies(s);
+        int commonFrequency = charFrequencies.get(s.charAt(0));
+        for (int frequency : charFrequencies.values()) {
+            if (frequency != commonFrequency) return false;
+        }
+        return true;
+    }
+
+    private Map<Character, Integer> getCharacterFrequencies(String s) {
+        final Map<Character, Integer> characterFrequencies = new HashMap<>();
+        for (int i = 0 ; i < s.length() ; i++) {
+            characterFrequencies.put(s.charAt(i), characterFrequencies.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        return characterFrequencies;
+    }
+}
