4d21dca69492a3259f7cfef5b3d7c77bc8b7c9f7
anishLearnsToCode <anish_@outlook.com>
2021-12-25 13:08:21 +0100

    solves find target indices after sorting

--- src/FindTargetIndicesAfterSortingArray.java
+++ src/FindTargetIndicesAfterSortingArray.java
@@ -1,16 +1,35 @@
+// https://leetcode.com/problems/find-target-indices-after-sorting-array
+// T: O(|nums|)
+// S: O(|nums|)
+
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FindTargetIndicesAfterSortingArray {
     public List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
-        final List<Integer> indices = new ArrayList<>();
-        for (int i = 0 ; i < nums.length ; i++) {
-            if (nums[i] == target) {
-                indices.add(i);
-            }
+        final Map<Integer, Integer> frequency = getFrequency(nums);
+        int index = 0;
+        for (int i = 0 ; i < target ; i++) {
+            index += frequency.getOrDefault(i, 0);
         }
-        return indices;
+        return indices(index, frequency.getOrDefault(target, 0));
+    }
+
+    private Map<Integer, Integer> getFrequency(int[] array) {
+        final Map<Integer, Integer> result = new HashMap<>();
+        for (int element : array) {
+            result.put(element, result.getOrDefault(element, 0) + 1);
+        }
+        return result;
+    }
+
+    private List<Integer> indices(int start, int length) {
+        final List<Integer> result = new ArrayList<>(length);
+        for (int i = 0 ; i < length ; i++) {
+            result.add(start++);
+        }
+        return result;
     }
 }
