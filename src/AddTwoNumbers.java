226d1e28d10f68bf9e1856c5c9ab08d9746a747d
anishLearnsToCode <anish_@outlook.com>
2021-12-25 15:22:19 +0100

    solves add two numbers

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/add-two-numbers
+// T: O(max(|l1|, |l2|))
+// S: O(max(|l1|, |l2|))
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode current = result;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            current.next = new ListNode();
+            current = current.next;
+            int sum = get(l1) + get(l2) + carry;
+            current.val = sum % 10;
+            carry = sum / 10;
+            l1 = next(l1);
+            l2 = next(l2);
+        }
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return result.next;
+    }
+
+    private ListNode next(ListNode node) {
+        if (node == null) return null;
+        return node.next;
+    }
+
+    private int get(ListNode node) {
+        if (node == null) return 0;
+        return node.val;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
