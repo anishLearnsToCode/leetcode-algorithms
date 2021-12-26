4b446ebb19d565392dc29fa0ded10333804c24a8
anishLearnsToCode <anish_@outlook.com>
2021-12-26 22:50:39 +0100

    solves swap nodes in pairs

---
+++ src/SwapNodesInPairs.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/swap-nodes-in-pairs
+// T: O(n)
+// S: O(1)
+
+public class SwapNodesInPairs {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = head.next, a = head, b = head.next.next, temp;
+        head.next.next = head;
+        while (a != null) {
+            a.next = (b == null || b.next == null ? b : b.next);
+            temp = (b == null || b.next == null ? null : b.next.next);
+            if (b != null && b.next != null) b.next.next = b;
+            a = b;
+            b = temp;
+        }
+        return newHead;
+    }
+}
