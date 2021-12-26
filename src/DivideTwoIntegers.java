542c263fc14a4d0e52af9290d7f7034c2f68d983
anishLearnsToCode <anish_@outlook.com>
2021-12-26 23:01:14 +0100

    solves divide two integers

---
+++ src/DivideTwoIntegers.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/divide-two-integers
+// T: O(A / B)
+// S: O(1)
+
+public class DivideTwoIntegers {
+    public int divide(int A, int B) {
+        if (A == 1 << 31 && B == -1) return (1 << 31) - 1;
+        int a = Math.abs(A), b = Math.abs(B), res = 0, x = 0;
+        while (a - b >= 0) {
+            for (x = 0; a - (b << x << 1) >= 0; x++);
+            res += 1 << x;
+            a -= b << x;
+        }
+        return (A > 0) == (B > 0) ? res : -res;
+    }
+}
