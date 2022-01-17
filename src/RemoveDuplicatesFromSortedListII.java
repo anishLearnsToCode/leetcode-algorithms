9fd0ab5924566925f84564e336e96adacc971d55
anishLearnsToCode <anish_@outlook.com>
2022-01-17 22:11:54 +0100

    solves remove duplicate from sorted list II

---
+++ src/RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii
+// T: O(N)
+// S: O(1)
+
+public class RemoveDuplicatesFromSortedListII {
+    private static class ListNode {
+        int val;
+        ListNode next;
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode tempHead = new ListNode();
+        tempHead.next = head;
+        int val;
+        for (ListNode current = head, previous = tempHead ; current != null && current.next != null ; ) {
+            if (current.val == current.next.val) {
+                val = current.val;
+                while (current.next != null && current.next.val == val) {
+                    current = current.next;
+                }
+                current = current.next;
+                previous.next = current;
+            } else {
+                previous = current;
+                current = current.next;
+            }
+        }
+        return tempHead.next;
+    }
+}
