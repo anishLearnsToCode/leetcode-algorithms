89f8d9df79c8ed058d1654db535c9b849a948d3d
anishLearnsToCode <anish_@outlook.com>
2022-01-17 22:42:00 +0100

    solves partition list

---
+++ src/PartitionList.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/partition-list
+// T: O(N)
+// S: O(1)
+
+public class PartitionList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+    }
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode beforeHead = new ListNode(), beforeTemp = beforeHead;
+        ListNode afterHead = new ListNode(), afterTemp = afterHead;
+        while (head != null) {
+            if (head.val < x) {
+                beforeTemp.next = head;
+                beforeTemp = beforeTemp.next;
+            } else {
+                afterTemp.next = head;
+                afterTemp = afterTemp.next;
+            }
+            head = head.next;
+        }
+        afterTemp.next = null;
+        beforeTemp.next = afterHead.next;
+        return beforeHead.next;
+    }
+}
