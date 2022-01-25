2e8e39993ff94c81252c9f52793e83d9807b8081
anishLearnsToCode <anish_@outlook.com>
2022-01-25 12:27:53 +0100

    a

--- src/ReverseLinkedListII.java
+++ src/ReverseLinkedListII.java
@@ -3,9 +3,27 @@ public class ReverseLinkedListII {
         int val;
         ListNode next;
         ListNode(int val) { this.val = val; }
+
+        @Override
+        public String toString() {
+            if (next == null) return "ListNode{val=" + val + ", next=null}";
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
     }
 
-    public ListNode reverseBetween(ListNode head, int leftVal, int rightVal) {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println(reverseBetween(head, 2, 4));
+    }
+
+    public static ListNode reverseBetween(ListNode head, int leftVal, int rightVal) {
         if (leftVal == rightVal) return head;
 
         ListNode sentinelHead = new ListNode(-1000);
@@ -13,7 +31,6 @@ public class ReverseLinkedListII {
         ListNode beforeLeft = getNodeBefore(sentinelHead, leftVal);
         System.out.println(beforeLeft.val);
         ListNode left = beforeLeft.next;
-//        ListNode afterRight = getNodeAfter(sentinelHead, rightVal);
         ListNode a = beforeLeft.next, b = a.next, c = b.next;
 
         while (c != null && b.val != rightVal) {
@@ -23,24 +40,16 @@ public class ReverseLinkedListII {
             b = c;
             c = c.next;
         }
-        a.next = null;
         b.next = a;
         beforeLeft.next = b;
         left.next = c;
         return sentinelHead.next;
     }
 
-    private ListNode getNodeBefore(ListNode head, int value) {
+    private static ListNode getNodeBefore(ListNode head, int value) {
         while (head != null && head.next != null && head.next.val != value) {
             head = head.next;
         }
         return head;
-    }
-
-    private ListNode getNodeAfter(ListNode head, int value) {
-        while (head != null && head.val != value) {
-            head = head.next;
-        }
-        return head.next;
     }
 }
