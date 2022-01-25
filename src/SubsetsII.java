8b2fe4a6af90b4187f2619ec584097c13087f804
anishLearnsToCode <anish_@outlook.com>
2022-01-25 11:23:56 +0100

    solves subsets II

--- src/SubsetsII.java
+++ src/SubsetsII.java
@@ -1,39 +1,27 @@
+// https://leetcode.com/problems/subsets-ii
+// T: O(2^n)
+// S: O(n * 2^n)
+
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class SubsetsII {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        final Map<Integer, Integer> frequencies = getElementFrequencies(nums);
+        Arrays.sort(nums);
         final List<List<Integer>> result = new ArrayList<>();
-        computeSubSets(frequencies, result, new LinkedList<>(), 0, nums.length);
+        helper(result, new LinkedList<>(), nums, 0);
         return result;
     }
 
-    private void computeSubSets(Map<Integer, Integer> frequencies, List<List<Integer>> result, LinkedList<Integer> current, int depth, int maxDepth) {
-        if (depth == maxDepth) {
-            result.add(new ArrayList<>(current));
-            return;
+    public void helper(List<List<Integer>> result, LinkedList<Integer> current, int[] numbers, int index) {
+        result.add(new ArrayList<>(current));
+        for(int i = index ; i < numbers.length ; i++) {
+            if(i > index && numbers[i] == numbers[i-1]) continue;
+            current.add(numbers[i]);
+            helper(result, current, numbers, i + 1);
+            current.removeLast();
         }
-        for (Map.Entry<Integer, Integer> entry : frequencies.entrySet()) {
-            if (entry.getValue() > 0) {
-                entry.setValue(entry.getValue() - 1);
-                computeSubSets(frequencies, result, current, depth + 1, maxDepth);
-                current.add(entry.getKey());
-                computeSubSets(frequencies, result, current, depth + 1, maxDepth);
-                current.removeLast();
-                entry.setValue(entry.getValue() + 1);
-            }
-        }
-    }
-
-    private Map<Integer, Integer> getElementFrequencies(int[] array) {
-        final Map<Integer, Integer> frequencies = new HashMap<>();
-        for (int element : array) {
-            frequencies.put(element, frequencies.getOrDefault(element, 0) + 1);
-        }
-        return frequencies;
     }
 }
