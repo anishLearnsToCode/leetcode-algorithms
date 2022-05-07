121971ba884e52e22cc125e3aec916bb1cf7f31d
Anish Sachdeva <anish_@outlook.com>
2022-05-07 21:30:54 +0200

    shifts tree node to personal class

--- src/MinimumDistanceBetweenBSTNodes.java
+++ src/MinimumDistanceBetweenBSTNodes.java
@@ -1,16 +1,3 @@
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 public class MinimumDistanceBetweenBSTNodes {
     long result = Integer.MAX_VALUE;
 
