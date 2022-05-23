b1246c4725080acb7e65549fab5da9e1dd526df0
Anish Sachdeva <anish_@outlook.com>
2022-05-23 19:56:29 +0200

    solves interleaving strings in java

--- src/InterleavingString.java
+++ src/InterleavingString.java
@@ -1,15 +1,34 @@
-public class InterleavingString {
-    public static void main(String[] args) {
-        System.out.println(isInterleave("aabcc", "dbbca", "aadbbcbcac"));
-    }
+// https://leetcode.com/problems/interleaving-string
+// T: O(n * m)
+// S: O(min(m, n))
 
-    public static boolean isInterleave(String s1, String s2, String s3) {
+public class InterleavingString {
+    public boolean isInterleave(String s1, String s2, String s3) {
         if (s1.length() + s2.length() != s3.length()) return false;
-        for (int i = 0, j = 0, t = 0 ; i <= s1.length() && j <= s2.length() && t < s3.length() ; t++) {
-            if (i < s1.length() && s3.charAt(t) == s1.charAt(i)) i++;
-            else if (j < s2.length() && s3.charAt(t) == s2.charAt(j)) j++;
-            else return false;
+        if (s1.length() < s2.length()) {
+            String temp = s1;
+            s1 = s2;
+            s2 = temp;
         }
-        return true;
+
+        final int rows = s1.length() + 1, columns = s2.length() + 1;
+        final boolean[] dp = new boolean[columns];
+
+        for (int row = 0 ; row < rows ; row++) {
+            for (int column = 0 ; column < columns ; column++) {
+                if (row == 0 && column == 0) {
+                    dp[column] = true;
+                } else if (row == 0) {
+                    dp[column] = dp[column - 1] && s2.charAt(column - 1) == s3.charAt(row + column - 1);
+                } else if (column == 0) {
+                    dp[column] = dp[column] && s1.charAt(row - 1) == s3.charAt(row + column - 1);
+                } else {
+                    dp[column] = (s1.charAt(row - 1) == s3.charAt(row + column - 1) && dp[column])
+                            || (s2.charAt(column - 1) == s3.charAt(row + column - 1) && dp[column - 1]);
+                }
+            }
+        }
+
+        return dp[columns - 1];
     }
 }
