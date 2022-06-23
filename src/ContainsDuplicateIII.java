88cdd6b5950963fa1766404e8706759eb255e376
anishLearnsToCode <anish_@outlook.com>
2022-06-23 09:27:14 +0200

    solves contains duplicate iii in java

--- src/ContainsDuplicateIII.java
+++ src/ContainsDuplicateIII.java
@@ -1,51 +1,34 @@
-import java.util.TreeMap;
+// https://leetcode.com/problems/contains-duplicate-iii
+// T: O(N)
+// S: O(N)
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ContainsDuplicateIII {
 
-    public boolean containsNearbyAlmostDuplicate(int[] array, int k, int t) {
-        // no memory
+    private long getID(long i, long w) {
+        return i < 0 ? (i + 1) / w - 1 : i / w;
+    }
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         if (k == 0) return false;
 
-        final TreeMap<Long, Integer> elements = new TreeMap<>();
-        for (int i = 0 ; i < array.length ; i++) {
-            long lowerBound = (long) array[i] - t;
-            long upperBound = (long) array[i] + t;
-            long lowerClosest = getClosestOrHigher(elements, lowerBound);
-            if (inRange(lowerClosest, lowerBound, upperBound)) return true;
-            long upperClosest = getClosestOrLower(elements, upperBound);
-            if (inRange(upperClosest, lowerBound, upperBound)) return true;
-            if (i >= k) {
-                removeFromMap(elements, array[i - k]);
-            }
-            addToMap(elements, array[i]);
+        final Map<Long, Long> buckets = new HashMap<>();
+        final long w = (long) t + 1;
+
+        for (int i = 0; i < nums.length; ++i) {
+            long m = getID(nums[i], w);
+            if (buckets.containsKey(m))
+                return true;
+            if (buckets.containsKey(m - 1) && Math.abs(nums[i] - buckets.get(m - 1)) < w)
+                return true;
+            if (buckets.containsKey(m + 1) && Math.abs(nums[i] - buckets.get(m + 1)) < w)
+                return true;
+            buckets.put(m, (long)nums[i]);
+            if (i >= k) buckets.remove(getID(nums[i - k], w));
         }
 
         return false;
-    }
-
-    private void removeFromMap(TreeMap<Long, Integer> elements, long val) {
-        if (elements.get(val) == 1) {
-            elements.remove(val);
-        } else {
-            elements.put(val, elements.get(val) - 1);
-        }
-    }
-
-    private void addToMap(TreeMap<Long, Integer> elements, long val) {
-        elements.put(val, elements.getOrDefault(val, 0) + 1);
-    }
-
-    private long getClosestOrHigher(TreeMap<Long, Integer> elements, long val) {
-        Long lowerClosest = elements.ceilingKey(val);
-        return lowerClosest == null ? Long.MAX_VALUE : lowerClosest;
-    }
-
-    private long getClosestOrLower(TreeMap<Long, Integer> elements, long val) {
-        Long upperClosest = elements.floorKey(val);
-        return upperClosest == null ? Long.MIN_VALUE : upperClosest;
-    }
-
-    private boolean inRange(long val, long low, long high) {
-        return low <= val && val <= high;
     }
 }
