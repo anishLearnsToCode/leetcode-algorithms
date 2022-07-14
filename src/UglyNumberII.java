5eed621725b4142973bf885364965f2968c2947f
Anish Sachdeva <anish_@outlook.com>
2022-07-14 13:59:30 +0200

    solves ugly number iii

--- src/UglyNumberII.java
+++ src/UglyNumberII.java
@@ -6,19 +6,18 @@ public class UglyNumberII {
     public int nthUglyNumber(int n) {
         final int[] dp = new int[n];
         dp[0] = 1;
-        int index2, index3, index5;
-        index2 = index3 = index5 = 0;
         int factor2 = 2, factor3 = 3, factor5 = 5;
+        int index2 = 0, index3 = 0, index5 = 0;
 
         for(int i = 1 ; i < n ; i++) {
             final int uglyNumber = min(factor2, factor3, factor5);
             dp[i] = uglyNumber;
-            if(factor2 == uglyNumber) factor2 = 2 * dp[++index2];
-            if(factor3 == uglyNumber) factor3 = 3 * dp[++index3];
-            if(factor5 == uglyNumber) factor5 = 5 * dp[++index5];
+            if (uglyNumber == factor2) factor2 = 2 * dp[++index2];
+            if (uglyNumber == factor3) factor3 = 3 * dp[++index3];
+            if (uglyNumber == factor5) factor5 = 5 * dp[++index5];
         }
 
-        return dp[n-1];
+        return dp[dp.length - 1];
     }
 
     private int min(int a, int b, int c) {
--- src/UglyNumberIII.java
+++ src/UglyNumberIII.java
@@ -1,28 +1,59 @@
-import java.util.Arrays;
+// https://leetcode.com/problems/ugly-number-iii
+// T: O(log(MAX_VAL)) MAX_VAL = Integer.MAX_VAL here
+// S: O(1)
 
 public class UglyNumberIII {
-    public static int nthUglyNumber(int n, int a, int b, int c) {
-        final int[] dp = new int[n + 1];
-        dp[0] = 1;
-        int factorA = a, factorB = b, factorC = c;
-        int indexA = 0, indexB = 0, indexC = 0;
+    private long a;
+    private long b;
+    private long c;
+    private long lcm_a_b;
+    private long lcm_a_c;
+    private long lcm_b_c;
+    private long lcm_a_b_c;
 
-        for (int i = 1 ; i < dp.length ; i++) {
-            final int uglyNumber = min(factorA, factorB, factorC);
-            dp[i] = uglyNumber;
-            if (uglyNumber == factorA) factorA = a * dp[++indexA];
-            if (uglyNumber == factorB) factorB = b * dp[++indexB];
-            if (uglyNumber == factorC) factorC = c * dp[++indexC];
+    public int nthUglyNumber(int n, int a, int b, int c) {
+        setValues(a, b, c);
+        int left = 1, right = Integer.MAX_VALUE, middle;
+        long factors;
+        while (left <= right) {
+            middle = left + (right - left) / 2;
+            factors = numberOfFactors(middle);
+            if (factors >= n) right = middle - 1;
+            else left = middle + 1;
         }
-        System.out.println(Arrays.toString(dp));
-        return dp[dp.length - 1];
+        return left;
     }
 
-    private static int min(int a, int b, int c) {
-        return Math.min(a, Math.min(b, c));
+    private void setValues(long a, long b, long c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.lcm_a_b = lcm(a, b);
+        this.lcm_a_c = lcm(a, c);
+        this.lcm_b_c = lcm(b, c);
+        this.lcm_a_b_c = lcm(lcm_a_b, c);
     }
 
-    public static void main(String[] args) {
-        System.out.println(nthUglyNumber(100, 2, 3, 5));
+    /*
+    * @param n number
+    * @return will tell how many factors are there of a, b and c between 1 and n
+    *           e.g. if n=10 and a=2 b=3 so there are 7 factors {2, 3, 4, 6, 8, 9, 10}
+     */
+    private long numberOfFactors(int n) {
+        return n / a
+               + n / b
+               + n / c
+               - n / lcm_a_b
+               - n / lcm_a_c
+               - n / lcm_b_c
+               + n / lcm_a_b_c;
+    }
+
+    private long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    private long lcm(long a, long b) {
+        return (a * b) / gcd(a, b);
     }
 }
