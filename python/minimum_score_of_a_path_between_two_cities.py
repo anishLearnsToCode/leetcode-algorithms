42bc4e0ce9897d285790d3fc3c5a6438ba234a2d
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-03-22 14:15:12 +0530

    2492: Minimum Score of a Path Between Two Cities

---
+++ python/minimum_score_of_a_path_between_two_cities.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def __init__(self):
+        self.min_dist = 1e14
+
+    def minScore(self, n: int, roads) -> int:
+        graph = {}
+        vis = [False]*n
+        for x, y, d in roads:
+            if x in graph:
+                graph[x].append((y, d))
+            else:
+                graph[x] = [(y, d)]
+            if y in graph:
+                graph[y].append((x, d))
+            else:
+                graph[y] = [(x, d)]
+
+        visited = set()
+        queue = deque([1])
+
+        while queue:
+            node = queue.popleft()
+            for adj, score in graph[node]:
+                if adj not in visited:
+                    queue.append(adj)
+                    visited.add(adj)
+                self.min_dist = min(self.min_dist, score)
+        return self.min_dist
