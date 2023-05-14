d831362b0efe28d64c94c14636977ae08d704a8a
Anish Sachdeva <anish_@outlook.com>
2023-05-14 19:34:56 +0200

    solves #2235: Add Two Integers in java

---
+++ src/AddTwoIntegers.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/add-two-integers
+// T: O(1)
+// S: O(1)
+
+public class AddTwoIntegers {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
