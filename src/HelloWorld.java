5b6ebaf00a2123b1155c6f82d7637fc6e0971da1
Anish Sachdeva <anish_@outlook.com>
2024-07-05 03:11:40 +0200

    solve #1637: WidestVerticalAreaBetweenTwoPointsContainingNoPoints in Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,32 +1,65 @@
-public class HelloWorld {
-    private final int[] memory;
+import java.util.Scanner;
 
-    public HelloWorld(int[] array, int m) {
-        this.memory = new int[m + 1];
-        for (int element : array) {
-            memory[element]++;
-        }
-        for (int i = 1 ; i < memory.length ; i++) {
-            memory[i] += memory[i - 1];
+public class HelloWorld {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private static void spiralOrder(int[][] matrix){
+        final int rows = matrix.length, columns = matrix[0].length, elements = rows * columns;
+        for (int i = 0, top = 0, bottom = rows, left = 0, right = columns ; ; ) {
+            for (int row = top, column = left ; column < right ; column++, i++) {
+                System.out.print(matrix[row][column]);
+            }
+            top++;
+            if (i == elements) break;
+            for (int row = top, column = right - 1 ; row < bottom ; row++, i++) {
+                System.out.print(matrix[row][column]);
+            }
+            right--;
+            if (i == elements) break;
+            for (int row = bottom - 1, column = right - 1 ; column >= left ; column--, i++) {
+                System.out.print(matrix[row][column]);
+            }
+            bottom--;
+            if (i == elements) break;
+            for (int row = bottom - 1, column = left ; row >= top ; row--, i++) {
+                System.out.print(matrix[row][column]);
+            }
+            left++;
+            if (i == elements) break;
         }
     }
 
-    public int query(int a, int b) {
-        if (b > memory.length - 1) {
-            b = memory.length - 1;
-        }
+    private static int intInput(final String string) {
+        System.out.print(string);
+        final int i = SCANNER.nextInt();
+        return i;
+    }
 
-        if (a <= 0) {
-            a = 1;
-        } else if (a > memory.length - 1) {
-            a = memory.length;
+    private static void print(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
         }
-
-        return this.memory[b] - this.memory[a - 1];
     }
 
     public static void main(String[] args) {
+        final int rows = intInput("Enter row size: ");
+        final int columns = intInput("Enter column size: ");
+        final int[][] matrix = new int[rows][columns];
 
-        System.out.println(Math.round(10.5));
+        System.out.println("Enter array elements:");
+
+        for (int i = 0 ; i < rows ; i++) {
+            for(int j = 0 ; j < columns ; j++) {
+                matrix[i][j] = SCANNER.nextInt();
+            }
+        }
+
+        spiralOrder(matrix);
+
+        System.out.println("\n\nOriginal matrix");
+        print(matrix);
     }
 }
---
+++ src/WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class WidestVerticalAreaBetweenTwoPointsContainingNoPoints {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(a -> a[0]));
+        int maxVerticalArea = 0;
+        for (int i = 0 ; i < points.length - 1; i++) {
+            maxVerticalArea = Math.max(maxVerticalArea, points[i + 1][0] - points[i][0]);
+        }
+        return maxVerticalArea;
+    }
+}
