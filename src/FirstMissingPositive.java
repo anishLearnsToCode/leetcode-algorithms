493d73630e3a25eef48a09a8423dda5d7f10dd8f
anishLearnsToCode <anish_@outlook.com>
2024-07-18 13:12:39 +0200

    solves #41: First Missing Positive in java

---
+++ src/FirstMissingPositive.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/first-missing-positive
+// T: O(N)
+// S: O(1)
+
+public class FirstMissingPositive {
+    public static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Use cycle sort to place positive elements smaller than n at the correct index
+        for (int i = 0 ; i < n ; ) {
+            if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
+            }
+        }
+
+        // Iterate through nums return smallest missing positive integer
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all elements are at the correct index
+        // the smallest missing positive number is n + 1
+        return n + 1;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstMissingPositive(new int[] {3, 4, -1, 1}));
+    }
+}
