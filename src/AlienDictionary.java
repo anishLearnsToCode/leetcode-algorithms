6c10ca9680bc44216d1809edfbeca46655ef9ecf
Anish Sachdeva <anish_@outlook.com>
2024-07-30 13:43:28 +0200

    solves #269: Alien Dictionary in java

--- src/AlienDictionary.java
+++ src/AlienDictionary.java
@@ -1,91 +1,68 @@
 // https://leetcode.com/problems/alien-dictionary
-// T: O()
-// S: O()
+// C = length of all words added together
+// T: O(C)
+// S: O(1)
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 public class AlienDictionary {
     public String alienOrder(String[] words) {
-        final Map<Character, Set<Character>> graph = createGraphFrom(words);
-        return lexicographicallySmallest(graph);
-    }
 
-    private static String lexicographicallySmallest(Map<Character, Set<Character>> graph) {
-        final Map<Character, Integer> inDegree = computeInDegree(graph);
-        final Queue<Character> queue = new LinkedList<>();
-        final StringBuilder builder = new StringBuilder();
-        add0InDegreeInQueue(queue, inDegree);
-
-        while (!queue.isEmpty()) {
-            final char c = queue.poll();
-            builder.append(c);
-            for (char neighbour : graph.getOrDefault(c, new HashSet<>())) {
-                inDegree.put(neighbour, inDegree.get(neighbour) - 1);
-                if (inDegree.get(neighbour) == 0) {
-                    queue.add(neighbour);
-                }
-            }
-            graph.remove(c);
-        }
-
-        if (graph.isEmpty()) {
-            return builder.toString();
-        }
-        return "";
-    }
-
-    private static Map<Character, Set<Character>> createGraphFrom(String[] words) {
-        final Map<Character, Set<Character>> graph = new HashMap<>();
-        addAllLettersInGraph(graph, words);
-        for (int i = 0 ; i < words.length ; i++) {
-            for (int j = i + 1 ; j < words.length ; j++) {
-                if (words[i].equals(words[j])) {
-                    continue;
-                }
-                final char[] charDiff = firstDifferentCharacter(words[i], words[j]);
-                if (charDiff.length == 0) {
-                    continue;
-                }
-                final Set<Character> set = graph.getOrDefault(charDiff[0], new HashSet<>());
-                set.add(charDiff[1]);
-                graph.putIfAbsent(charDiff[0], set);
-            }
-        }
-        return graph;
-    }
-
-    private static void addAllLettersInGraph(Map<Character, Set<Character>> graph, String[] words) {
+        // Step 0: Create data structures and find all unique letters.
+        Map<Character, List<Character>> adjList = new HashMap<>();
+        Map<Character, Integer> counts = new HashMap<>();
         for (String word : words) {
-            for (int i = 0 ; i < word.length() ; i++) {
-                graph.putIfAbsent(word.charAt(i), new HashSet<>());
+            for (char c : word.toCharArray()) {
+                counts.put(c, 0);
+                adjList.put(c, new ArrayList<>());
             }
         }
-    }
 
-    private static char[] firstDifferentCharacter(String s1, String s2) {
-        for (int i = 0 ; i < s1.length() && i < s2.length() ; i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                return new char[] { s1.charAt(i), s2.charAt(i) };
+        // Step 1: Find all edges.
+        for (int i = 0; i < words.length - 1; i++) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+            // Check that word2 is not a prefix of word1.
+            if (word1.length() > word2.length() && word1.startsWith(word2)) {
+                return "";
+            }
+            // Find the first non match and insert the corresponding relation.
+            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
+                if (word1.charAt(j) != word2.charAt(j)) {
+                    adjList.get(word1.charAt(j)).add(word2.charAt(j));
+                    counts.put(word2.charAt(j), counts.get(word2.charAt(j)) + 1);
+                    break;
+                }
             }
         }
-        return new char[] {};
-    }
 
-    private static Map<Character, Integer> computeInDegree(Map<Character, Set<Character>> graph) {
-        final Map<Character, Integer> inDegree = new HashMap<>();
-        for (Set<Character> set : graph.values()) {
-            for (char c : set) {
-                inDegree.put(c, inDegree.getOrDefault(c, 0) + 1);
+        // Step 2: Breadth-first search.
+        StringBuilder sb = new StringBuilder();
+        Queue<Character> queue = new LinkedList<>();
+        for (Character c : counts.keySet()) {
+            if (counts.get(c).equals(0)) {
+                queue.add(c);
             }
         }
-        return inDegree;
-    }
+        while (!queue.isEmpty()) {
+            Character c = queue.remove();
+            sb.append(c);
+            for (Character next : adjList.get(c)) {
+                counts.put(next, counts.get(next) - 1);
+                if (counts.get(next).equals(0)) {
+                    queue.add(next);
+                }
+            }
+        }
 
-    private static void add0InDegreeInQueue(Queue<Character> queue, Map<Character, Integer> inDegree) {
-        for (Map.Entry<Character, Integer> entry : inDegree.entrySet()) {
-            if (entry.getValue() == 0) {
-                queue.add(entry.getKey());
-            }
+        if (sb.length() < counts.size()) {
+            return "";
         }
+        return sb.toString();
     }
 }
